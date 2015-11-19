7b2bb17c84ad00126da7a12950e15facc96a5d62
Yefim-Krokhin <pechenkaya@mail.ru>
2015-11-19 16:12:48 +0300

    app with movements detection

---
+++ app/src/main/java/com/infosec/MainActivity.java
@@ -0,0 +1,148 @@
+package com.infosec;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by Krokhin on 12.11.2015.
+ */
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private float lastX, lastY, lastZ;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private int amountOfShakes = 0;
+    private String clazz = "MainActivity";
+    private float deltaXMax = 0;
+    private float deltaYMax = 0;
+    private float deltaZMax = 0;
+
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private float vibrateThreshold = 0;
+
+    private TextView currentX, currentY, currentZ, maxX, maxY, maxZ;
+
+    public Vibrator v;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeViews();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//			TODO may be change delay
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            vibrateThreshold = accelerometer.getMaximumRange() / 3;
+        }
+        v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+    }
+
+    public void initializeViews() {
+        currentX = (TextView) findViewById(R.id.currentX);
+        currentY = (TextView) findViewById(R.id.currentY);
+        currentZ = (TextView) findViewById(R.id.currentZ);
+
+        maxX = (TextView) findViewById(R.id.maxX);
+        maxY = (TextView) findViewById(R.id.maxY);
+        maxZ = (TextView) findViewById(R.id.maxZ);
+    }
+
+    protected void onResume() {
+        super.onResume();
+//        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    //onPause() unregister the accelerometer for stop listening the events
+    protected void onPause() {
+        super.onPause();
+//        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        displayCleanValues();
+        displayCurrentValues();
+        displayMaxValues();
+
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        if (deltaX < 2)
+            deltaX = 0;
+        if (deltaY < 2)
+            deltaY = 0;
+        if (deltaZ < 2)
+            deltaZ = 0;
+        // set the last know values of x,y,z
+        lastX = event.values[0];
+        lastY = event.values[1];
+        lastZ = event.values[2];
+
+        vibrate();
+
+    }
+
+    public void vibrate() {
+//        TODO start here timer and if mobile moves too long send block
+        if ((deltaX > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
+            v.vibrate(50);
+            amountOfShakes++;
+        }
+        if (amountOfShakes > 4) {
+            Log.d(clazz, "BLOCK");
+            amountOfShakes = 0;
+        }
+    }
+
+    public void displayCleanValues() {
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+    }
+
+    // display the current x,y,z accelerometer values
+    public void displayCurrentValues() {
+        currentX.setText(Float.toString(deltaX));
+        currentY.setText(Float.toString(deltaY));
+        currentZ.setText(Float.toString(deltaZ));
+    }
+
+    // display the max x,y,z accelerometer values
+    public void displayMaxValues() {
+        if (deltaX > deltaXMax) {
+            deltaXMax = deltaX;
+            maxX.setText(Float.toString(deltaXMax));
+        }
+        if (deltaY > deltaYMax) {
+            deltaYMax = deltaY;
+            maxY.setText(Float.toString(deltaYMax));
+        }
+        if (deltaZ > deltaZMax) {
+            deltaZMax = deltaZ;
+            maxZ.setText(Float.toString(deltaZMax));
+        }
+    }
+
+}
